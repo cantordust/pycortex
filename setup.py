81207d7bade13be3f72659855e05394a518b8ece
Cantor Dust <1425730-cantordust@users.noreply.gitlab.com>
2018-12-24 05:27:23 +1100

    ===[ Updated LICENCE.md, setup.py ]===

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='cortex',
-                 version='0.1',
+                 version='0.1.1',
                  description='Library for evolving deep learning models in PyTorch',
                  license='MIT',
                  packages=setuptools.find_packages(),
