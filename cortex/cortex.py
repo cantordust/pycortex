5dc6d75341c9cb30ab72a8e4bbec7465c844c105
Cantor Dust <1425730-cantordust@users.noreply.gitlab.com>
2018-12-28 16:47:52 +1100

    ===[ Added Species.Offspring counter ]=== - Now the ecosystem cannot replace more than half of the networks. This is done to allow the cull() routine to work properly and to ensure some continuity. Without a limit, cull() could potentially remove all parents and start culling children, which would be pointless).

--- cortex/cortex.py
+++ cortex/cortex.py
@@ -180,12 +180,12 @@ def init():
     print(">>> Initialising ecosystem...")
 
     # Reset the static network attributes
-    Net.champ = None
-    Net.ecosystem.clear()
+    Net.Champion = None
+    Net.Ecosystem.clear()
     Net.ID = 0
 
     # Reset the static species attributes
-    Species.populations.clear()
+    Species.Populations.clear()
     Species.ID = 0
 
     # Sanity check on the species count
@@ -223,7 +223,7 @@ def init():
             for n in range(net_quota):
                 proto_net = Net(_species = proto_species)
 
-            if len(Species.populations) == Species.Init.Count:
+            if len(Species.Populations) == Species.Init.Count:
                 break
 
             proto_net = Net(_species = proto_species, _isolated = True)
@@ -236,12 +236,12 @@ def init():
             proto_net = Net()
             proto_net.mutate()
 
-    print("Species:", len(Species.populations))
-    for species in Species.populations.values():
+    print("Species:", len(Species.Populations))
+    for species in Species.Populations.values():
         species.print()
 
-    print("Nets:", len(Net.ecosystem))
-    for net in Net.ecosystem.values():
+    print("Nets:", len(Net.Ecosystem))
+    for net in Net.Ecosystem.values():
         net.print()
 
     if not UnitTestMode:
@@ -256,19 +256,19 @@ def init():
 def calibrate():
 
     # Remove extinct species.
-    extinct = [species.ID for species in Species.populations.values() if len(species.nets) == 0]
+    extinct = [species.ID for species in Species.Populations.values() if len(species.nets) == 0]
 
     while len(extinct) > 0:
-        del Species.populations[extinct.pop()]
+        del Species.Populations[extinct.pop()]
 
     # Increase and normalise the age of all networks.
     net_stat = Stat.SMAStat()
-    for net in Net.ecosystem.values():
+    for net in Net.Ecosystem.values():
         net.age += 1
         net_stat.update(net.age)
 
-    # Reset the champion.
-    Net.champ = None
+    # Reset the global champion.
+    Net.Champion = None
 
     # Running statistics about species fitness
     species_stat = Stat.SMAStat()
@@ -278,7 +278,7 @@ def calibrate():
 
     complexity_fitness_scale = {}
     complexity_stat = Stat.SMAStat()
-    for net in Net.ecosystem.values():
+    for net in Net.Ecosystem.values():
         complexity_fitness_scale[net.ID] = net.get_parameter_count()
         complexity_stat.update(complexity_fitness_scale[net.ID])
 
@@ -289,20 +289,20 @@ def calibrate():
         complexity_fitness_scale[net_id] = complexity_stat.get_inv_offset(complexity_fitness_scale[net_id])
         print("Network", net_id, "fitness scale:", complexity_fitness_scale[net_id])
 
-    for species in Species.populations.values():
+    for species in Species.Populations.values():
 
         # Compute the relative network fitness
         # and the absolute species fitness.
         species.calibrate(complexity_fitness_scale)
         species_stat.update(species.fitness.absolute)
 
-        if (Net.champ is None or
-            Net.ecosystem[species.champ].fitness.absolute > top_fitness):
+        if (Net.Champion is None or
+            Net.Ecosystem[species.champion].fitness.absolute > top_fitness):
 
-            Net.champ = species.champ
-            top_fitness = Net.ecosystem[species.champ].fitness.absolute
+            Net.Champion = species.champion
+            top_fitness = Net.Ecosystem[species.champion].fitness.absolute
 
-    for species in Species.populations.values():
+    for species in Species.Populations.values():
         # Compute the relative species fitness
         species.fitness.relative = species_stat.get_offset(species.fitness.absolute)
         species.print()
@@ -319,40 +319,40 @@ def save(_net_id,
     else:
         name = _name
 
-    torch.save(Net.ecosystem[_net_id], save_dir + '/' + name + '.pt')
+    torch.save(Net.Ecosystem[_net_id], save_dir + '/' + name + '.pt')
 
-#    Net.ecosystem[_net_id].print(save_dir + '/' + name + '.txt')
+#    Net.Ecosystem[_net_id].print(save_dir + '/' + name + '.txt')
 
 def cull():
 
-    while len(Net.ecosystem) > Net.Max.Count:
+    while len(Net.Ecosystem) > Net.Max.Count:
 
         # Get a random species ID
         species_wheel = Rand.RouletteWheel(Rand.WeightType.Inverse)
-        for species in Species.populations.values():
+        for species in Species.Populations.values():
             species_wheel.add(species.ID, species.fitness.relative)
 
         species_id = species_wheel.spin()
 
         # Get a random network ID
-        net_wheel = Rand.RouletteWheel()
-        for net_id in Species.populations[species_id].nets:
-            net = Net.ecosystem[net_id]
+        net_wheel = Rand.RouletteWheel(Rand.WeightType.Inverse)
+        for net_id in Species.Populations[species_id].nets:
+            net = Net.Ecosystem[net_id]
             if net.age > 0:
-                net_wheel.add(net_id, 1.0 / Net.ecosystem[net_id].fitness.relative)
+                net_wheel.add(net_id, Net.Ecosystem[net_id].fitness.relative)
 
         net_id = net_wheel.spin()
 
         print("Erasing network ", net_id)
 
         # Erase the network from the species.
-        Species.populations[species_id].nets.remove(net_id)
+        Species.Populations[species_id].nets.remove(net_id)
 
         # Erase the network from the ecosystem.
-        del Net.ecosystem[net_id]
+        del Net.Ecosystem[net_id]
 
-        if len(Species.populations[species_id].nets) == 0:
-            del Species.populations[species_id]
+        if len(Species.Populations[species_id].nets) == 0:
+            del Species.Populations[species_id]
 
 def evolve(_stats):
 
@@ -362,8 +362,11 @@ def evolve(_stats):
     print("\t`-> Calibrating...")
     calibrate()
 
+    # Set the offspring count to 0
+    Species.Offspring = 0
+
     if not UnitTestMode:
-        for net in Net.ecosystem.values():
+        for net in Net.Ecosystem.values():
 
 #            Logger.add_scalars('Stats for network ' + str(Net.ID), {
 #                    'Absolute fitness': net.fitness.absolute,
@@ -374,21 +377,21 @@ def evolve(_stats):
 #            CurrentEpoch)
             save(net.ID)
 
-        Logger.add_scalar('Highest fitness', Net.ecosystem[Net.champ].fitness.absolute, CurrentEpoch)
-        Logger.add_scalar('Networks', len(Net.ecosystem), CurrentEpoch)
-        Logger.add_scalar('Species', len(Species.populations), CurrentEpoch)
+        Logger.add_scalar('Highest fitness', Net.Ecosystem[Net.Champion].fitness.absolute, CurrentEpoch)
+        Logger.add_scalar('Networks', len(Net.Ecosystem), CurrentEpoch)
+        Logger.add_scalar('Species', len(Species.Populations), CurrentEpoch)
 
-        if Net.champ is not None:
-            save(Net.champ, 'champion')
+        if Net.Champion is not None:
+            save(Net.Champion, 'champion')
 
-        _stats['Parameters'].update(Net.ecosystem[Net.champ].get_parameter_count())
-        _stats['Accuracy'].update(Net.ecosystem[Net.champ].fitness.absolute)
+        _stats['Parameters'].update(Net.Ecosystem[Net.Champion].get_parameter_count())
+        _stats['Accuracy'].update(Net.Ecosystem[Net.Champion].fitness.absolute)
 
         if CurrentEpoch < Epochs - 1:
             # Evolve networks in each species.
             print("\t`-> Evolving networks...")
-            for species_id in list(Species.populations.keys()):
-                Species.populations[species_id].evolve()
+            for species_id in list(Species.Populations.keys()):
+                Species.Populations[species_id].evolve()
 
             # Eliminate unfit networks and empty species.
             print("\t`-> Culling...")
@@ -496,16 +499,16 @@ def run():
             print("\t`-> Evaluating networks...")
 
             with context.Pool(processes = MaxThreads) as pool:
-                results = pool.starmap(TrainFunction, zip(Net.ecosystem.values(), [CurrentEpoch] * len(Net.ecosystem), [DataDir] * len(Net.ecosystem)))
+                results = pool.starmap(TrainFunction, zip(Net.Ecosystem.values(), [CurrentEpoch] * len(Net.Ecosystem), [DataDir] * len(Net.Ecosystem)))
 
             for net in results:
-                Net.ecosystem[net.ID] = net
+                Net.Ecosystem[net.ID] = net
 
 #            ecosystem = tm.Manager().dict()
 #            processes = []
 #
 #            # Dispatch
-#            for net in Net.ecosystem.values():
+#            for net in Net.Ecosystem.values():
 #                processes.append(context.Process(target=TrainFunction, args=(net, CurrentEpoch, ecosystem, DataDir)))
 #                processes[-1].start()
 #
@@ -514,12 +517,12 @@ def run():
 #                process.join()
 #
 #            for net_id, net in ecosystem.items():
-#                Net.ecosystem[net_id] = net
-#            print("Ecosystem size:", len(Net.ecosystem))
+#                Net.Ecosystem[net_id] = net
+#            print("Ecosystem size:", len(Net.Ecosystem))
 
             evolve(stats)
 
-        for net_id in Net.ecosystem.keys():
+        for net_id in Net.Ecosystem.keys():
             save(net_id)
 
     with open(LogDir + '/config.txt', 'w') as cfg_file:
