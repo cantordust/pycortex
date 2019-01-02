0864a7acdabc38ca01e5f52d48b91fc3d7546ca3
Cantor Dust <1425730-cantordust@users.noreply.gitlab.com>
2019-01-03 10:50:03 +1100

    ===[ Parameter detachment issue ]===

--- cortex/layer.py
+++ cortex/layer.py
@@ -788,10 +788,10 @@ class Layer(tn.Module):
 
     def overlay_kernels(self):
 
-#        self.weight = self.weight.detach()
+        self.weight = self.weight.detach()
 
         for output_node in range(len(self.nodes)):
-            self.weight[output_node][self.weight_slices[output_node]] = self.nodes[output_node].detach()
+            self.weight[output_node][self.weight_slices[output_node]] = self.nodes[output_node].clone()
 
     def forward(self,
                 _tensor):
